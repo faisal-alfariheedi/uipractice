c0b2983339ee99b0c29ac79edd2836488cd905c3
Faisal <falfariheedi@gmail.com>
2021-10-18 14:07:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/uipractice/fb.kt
@@ -0,0 +1,12 @@
+
+package com.example.uipractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fb : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fb)
+    }
+}
